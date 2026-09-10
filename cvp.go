e91d0bc02b43541d382047669881858924b63f97
Ademighty2807 <202931993+Ademighty2807@users.noreply.github.com>
2026-09-10 12:44:57 +0100

    Concorrency snd parallelism

--- cvp.go
+++ cvp.go
@@ -2,30 +2,53 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+	"sync"
 	"time"
 )
 
-func printNumbers() {
-	for i := range 5 {
-		fmt.Println(time.Now())
-		fmt.Println(i)
-		time.Sleep(500 * time.Millisecond)
-	}
-}
+// func printNumbers() {
+// 	for i := range 5 {
+// 		fmt.Println(time.Now())
+// 		fmt.Println(i)
+// 		time.Sleep(500 * time.Millisecond)
+// 	}
+// }
 
-func printLetters() {
-	for _, letter := range "ABCDE" {
-		fmt.Println(time.Now())
-		fmt.Println(string(letter))
-		time.Sleep(500 * time.Millisecond)
+// func printLetters() {
+// 	for _, letter := range "ABCDE" {
+// 		fmt.Println(time.Now())
+// 		fmt.Println(letter)
+// 		time.Sleep(500 * time.Millisecond)
+// 	}
+// }
+
+func heavyTask(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Printf("Task %d is starting\n", id)
+	fmt.Println(time.Now())
+	for range 100_000_000 {
 	}
+	fmt.Printf("Task %d is finished\n", id)
+	fmt.Println(time.Now())
 }
 
 func main() {
 
-	go printNumbers()
-	go printNumbers()
+	// go printNumbers()
+	// go printNumbers()
 
-	time.Sleep(2 * time.Second)
+	// time.Sleep(5 * time.Second)
+
+	numThreads := 12
+
+	runtime.GOMAXPROCS(numThreads)
+	var wg sync.WaitGroup
+
+	for i := range numThreads {
+		wg.Add(1)
+		go heavyTask(i, &wg)
+	}
+	wg.Wait()
 
 }
